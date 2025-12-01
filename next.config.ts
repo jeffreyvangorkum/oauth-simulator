e63286aee4c5d2a1751a82029f9078362e87652f
Asaki2769 <jeffrey@van-gorkum.com>
2025-12-01 13:16:43 +0100

    chore: Update package version and refine build version detection to prioritize package.json.

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,16 @@ import type { NextConfig } from "next";
 
 import { execSync } from "child_process";
 
-let version = "0.0.0";
+import packageJson from "./package.json";
+
+let version = packageJson.version;
 try {
-  version = execSync("git describe --tags").toString().trim();
+  const gitVersion = execSync("git describe --tags").toString().trim();
+  if (gitVersion) {
+    version = gitVersion;
+  }
 } catch (e) {
-  console.warn("Failed to fetch git tag version", e);
+  // console.warn("Failed to fetch git tag version", e);
 }
 
 const nextConfig: NextConfig = {
